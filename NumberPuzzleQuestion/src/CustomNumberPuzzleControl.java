08a5fbe05dd11abfe5a16bc19a538024cda26a48
Ramya <Ramya@DESKTOP-MTB53IG>
2021-07-07 15:46:30 +0530

    Added required code to run the game

--- NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/CustomNumberPuzzleControl.java
@@ -1,5 +1,5 @@
 import java.awt.*;
-
+import java.util.*;
 class CustomNumberPuzzleControl extends NumberPuzzleControl {
 	public int getWidth() {
 		return 200;
@@ -36,17 +36,39 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		Button buttonClicked = game.getButtonClicked();
 		Button[] buttons = game.getButtons();
 		
-		//Your logic here		
-		
-		return emptyCellId;
+		//Your logic here	
+		int idx = 0;
+		for(int i=0;i<=15;i++) {
+			if(buttons[i]==buttonClicked) {
+				idx = i;
+				break;
+			}
+		}
+
+		if(idx+1 == emptyCellId || idx-1 == emptyCellId || idx+4 == emptyCellId || idx-4 == emptyCellId) {
+			swapButton(buttons[emptyCellId], buttonClicked);
+			return idx;
+		}
+		else {
+			return emptyCellId;
+		}
 
 	}
 	public int[] getRandomNumbersForGrid() {
 		int arr[] = new int[15];
 		
 		//Your logic here
-		int a = getRandomNumber();
 		
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		while(numbers.size()!=15) {
+			int a = getRandomNumber();
+			if(!numbers.contains(a%16)&&(a%16)!=0) {
+				numbers.add(a%16);
+			}
+		}
+		for(int i=0;i<15;i++) {
+			arr[i]=numbers.get(i);
+		}
 		
 		return arr;
 	}
@@ -55,7 +77,13 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 		boolean winner = true;
 		
 		// Your Logic here
-		getIntegerArrayOfButtonIds(buttons);
+		int[] numbers = getIntegerArrayOfButtonIds(buttons);
+		for(int i=0;i<numbers.length;i++) {
+			if(numbers[i]!=i+1) {
+				winner=false;
+				break;
+			}
+		}
 
 		return winner;
 	}
